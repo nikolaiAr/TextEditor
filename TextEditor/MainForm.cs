c285d58a8265b8e32158ab9a0a33d50c1b7f9c7a
nikolaiAr <nikolai.aronov@gmail.com>
2018-08-21 13:01:00 +0300

    view

--- TextEditor/MainForm.cs
+++ TextEditor/MainForm.cs
@@ -10,11 +10,88 @@ using System.Windows.Forms;
 
 namespace TextEditor
 {
-    public partial class MainForm : Form
+    public interface IMainForm
+    {
+        string FilePath { get; }
+        string Content { get; set; }
+        void SetSymbolCount(int count);
+        event EventHandler FileOpenClick;
+        event EventHandler FileSaveClick;
+        event EventHandler ContentChanged;
+    }
+    public partial class MainForm : Form, IMainForm
     {
         public MainForm()
         {
             InitializeComponent();
+            butOpenFile.Click += ButOpenFile_Click;
+            butSaveFile.Click += ButSaveFile_Click;
+            textBox.TextChanged += TextBox_TextChanged;
+            butSelectFile.Click += ButSelectFile_Click;
+            numFont.ValueChanged += numFont_ValueChanged;
+        }
+
+
+        #region Проброс событий
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (ContentChanged != null)
+                ContentChanged(this, EventArgs.Empty);
+        }
+
+        private void ButSaveFile_Click(object sender, EventArgs e)
+        {
+            if (FileSaveClick != null)
+                FileSaveClick(this, EventArgs.Empty);
+        }
+
+        private void ButOpenFile_Click(object sender, EventArgs e)
+        {
+            if (FileOpenClick != null)
+                FileOpenClick(this, EventArgs.Empty);
+        }
+        #endregion
+
+        #region IMainForm
+        public string FilePath
+        {
+            get { return filePath.Text; }
+        }
+
+        public string Content
+        {
+            get { return textBox.Text; }
+            set { textBox.Text = value; }
+        }
+
+        public void SetSymbolCount(int count)
+        {
+            lblSymbolCount.Text = count.ToString();
+        }
+
+        public event EventHandler FileOpenClick;
+        public event EventHandler FileSaveClick;
+        public event EventHandler ContentChanged;
+        #endregion
+
+        //обработчик выбора файла
+        private void ButSelectFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Текстовые файлы|*.txt|Все файлы|*.*";
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                filePath.Text = dlg.FileName;
+                if (FileOpenClick != null)
+                    FileOpenClick(this, EventArgs.Empty);
+            }
+        }
+
+        //выбор шрифта
+        private void numFont_ValueChanged(object sender, EventArgs e)
+        {
+            textBox.Font = new Font("Calibri", (float)numFont.Value);
         }
     }
 }
